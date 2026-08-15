deb59da7b30a472e2dbd628ef3b6c7ad303044cb
jango100500-ui <jango100500@gmail.com>
2026-08-15 14:45:07 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { PhysicsPills } from './components/PhysicsPills';
 
 export const App: React.FC = () => {
+  useEffect(() => {
+    // Блокировка ориентации на поддерживаемых устройствах
+    if (window.screen?.orientation && 'lock' in window.screen.orientation) {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      (window.screen.orientation as any).lock('portrait').catch(() => {
+        // Игнорируем ошибку, если браузер не поддерживает строгую блокировку
+      });
+    }
+  }, []);
+
   const handleStart = () => {
-    console.log('Игрок загадал персонажа!');
+    console.log('Игрок нажал "Я загадал!"');
   };
 
   return (
     <>
+      {/* Предупреждение при горизонтальном повороте */}
+      <div className="landscape-lock-overlay">
+        <h2>Пожалуйста, поверните устройство</h2>
+        <p>Приложение работает только в вертикальном режиме</p>
+      </div>
+
       <PhysicsPills />
 
       <main className="screen-container">
